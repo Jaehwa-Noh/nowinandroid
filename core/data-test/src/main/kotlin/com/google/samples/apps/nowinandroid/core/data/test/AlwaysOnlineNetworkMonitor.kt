3878e6508c301e0e7ed303a8e531267e119752e2
Jaehwa Noh <shwoghk14@gmail.com>
2024-03-28 21:29:49 +0900

    Revert "add AlwaysOfflineNetworkMonitor"
    Change-Id: I0e56350f2e6948db5468dbe07f3719647f911282

--- core/data-test/src/main/kotlin/com/google/samples/apps/nowinandroid/core/data/test/AlwaysOnlineNetworkMonitor.kt
+++ core/data-test/src/main/kotlin/com/google/samples/apps/nowinandroid/core/data/test/AlwaysOnlineNetworkMonitor.kt
@@ -14,12 +14,13 @@
  * limitations under the License.
  */
 
-package com.google.samples.apps.nowinandroid.core.data.test.networkmonitor
+package com.google.samples.apps.nowinandroid.core.data.test
 
 import com.google.samples.apps.nowinandroid.core.data.util.NetworkMonitor
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
+import javax.inject.Inject
 
-class AlwaysOfflineNetworkMonitor : NetworkMonitor {
-    override val isOnline: Flow<Boolean> = flowOf(false)
+class AlwaysOnlineNetworkMonitor @Inject constructor() : NetworkMonitor {
+    override val isOnline: Flow<Boolean> = flowOf(true)
 }
